0b8f2f1909eeb8c126cf48c395462f37f0f11ffb
mikayzo <d.mikalkenas@gmail.com>
2019-05-23 15:20:03 +0300

    Add hello.c to week0

---
+++ week0/hello.c
@@ -0,0 +1,12 @@
+// Dziugas Mikalkenas
+// 25 March 2019
+// Problem Set 1
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string name = get_string("What is your name?\n");
+    printf("hello, %s\n", name);
+}
